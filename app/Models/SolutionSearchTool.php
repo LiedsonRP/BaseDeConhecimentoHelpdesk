bf92b25874a8270acecf48a691f5c26f93422385
Liedson <liedsonreis2019@outlook.com>
2023-05-30 17:08:21 -0300

    Correção de bug da paginação

--- app/Models/SolutionSearchTool.php
+++ app/Models/SolutionSearchTool.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Collator;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Query\Builder;
@@ -26,14 +27,35 @@ class SolutionSearchTool
     /**
      * Retorna os resultados da pesquisa na forma de uma collection, retirando os resultados duplicados
      * 
-     * @return Illuminate\Support\Collection
+     * @param int $items_number Número de items na paginação
+     * @return array
      */
-    public function getResult() : Collection
+    public function getResult(int $items_number) : array
     {
-        $results = $this->builder->select()->distinct(["title"])->get()->pluck("solution_number")->sortDesc();
+        $database_pagination = $this->builder->select()->distinct(["title"])->paginate($items_number);
+        $id_collection = collect($database_pagination->items());        
+        $results = $this->format_solution_results($id_collection);
         
-        $solutions = $results->map(function($id) {
-            $solution = Solution::find($id)->load("categories");                
+        return ["data"=>$results, 
+        "current_page" => $database_pagination->currentPage(),
+        "last_page" => $database_pagination->lastPage()];
+    }
+
+    /**
+     * Formata os resultados da pesquisa na view para o padrão aceito no projeto
+     * @param Illuminate\Support\Collection
+     * @return Collection 
+     */
+    private function format_solution_results(Collection $solution_list) : Collection
+    {        
+        return $solution_list->map(function($solution_record) {            
+            $solution = new Solution([
+                "id" => $solution_record->solution_number,
+                "title" => $solution_record->title,
+                "created_at" => $solution_record->created_at,
+                "updated_at" => $solution_record->updated_at,
+            ]);                
+            $solution->load("categories");
             
             $categories  = $solution->categories()->get()->map(function (Category $category) {
                 return new Category($category->attributesToArray());
@@ -43,8 +65,6 @@ class SolutionSearchTool
             
             return $solution;
         });        
-
-        return $solutions;
     }
 
     /**
@@ -72,9 +92,13 @@ class SolutionSearchTool
      * Retorna uma solução cadastrada na base de dados junto de suas categorias e dados
      * 
      * @param int $id Número de identificação no banco de dados
+     * @return Solution
      */
-    public function getSingleSolution(int $id) : void
+    public function getSingleSolution(int $id) : Solution
     {
         $this->builder->where("solution_number", $id);                
+        $result = $this->getResult(1);
+        return $result["data"]->last();
+                
     }
 }
